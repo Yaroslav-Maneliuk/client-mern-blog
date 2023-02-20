a547a75edd2447a5fbee6c71795e654f0a384da9
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2023-02-20 10:43:19 +0200

    create client

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { formatISO9075 } from 'date-fns'
+import { Link } from 'react-router-dom';
+
+const Post = ({ _id, title, summary, cover, createdAt, author }) => {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt="img" />
+        </Link>
+      </div>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <time>{formatISO9075(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Post
